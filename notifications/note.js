d8a76fa79b08256391ffc8ba1e15fc9a1ddb9b66
Arcanus 55 Privacy Paranoid Vault <41128567+arcanus55@users.noreply.github.com>
2019-04-18 06:16:36 -0500

    Update note.js

--- notifications/note.js
+++ notifications/note.js
@@ -1,4 +1,4 @@
 'use strict';
 console.log("ducks");
 snck.q("Sorry, the Tax Day Sale is over|Next huge sale is");
-snck.q("National Password Day | May 3");
+snck.q("National Password Day | May 3 (maybe)");
