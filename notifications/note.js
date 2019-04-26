beb09c8afccb863d19245bafa236d4531282dafc
Arcanus 55 Privacy Paranoid Vault <41128567+arcanus55@users.noreply.github.com>
2019-04-25 19:46:08 -0500

    Update note.js

--- notifications/note.js
+++ notifications/note.js
@@ -1,3 +1,3 @@
 'use strict';
 snck.q("World Password Day|May 2nd 2019");
-snck.q("50% off all products!|Get the most paranoid|Password Manager!");
+snck.q("50% off all products!|Get the most paranoid Password Manager!");
