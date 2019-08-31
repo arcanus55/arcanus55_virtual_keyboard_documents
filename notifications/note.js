270ef740460492cd3d5c670e32c037c173ec533d
Arcanus 55 Privacy Paranoid Vault <41128567+arcanus55@users.noreply.github.com>
2019-08-30 21:14:17 -0500

    Update note.js

--- notifications/note.js
+++ notifications/note.js
@@ -1,6 +1,9 @@
 'use strict';
-snck.q("Can you keep a Secret?|Protect What is Important.");
+snck.q("Can you keep a Secret?|Protect What is MOST Important.");
 snck.q("Secret Now. Forever Secret.");
 setTimeout(function(){
-  snck.q("THERE IS NO MORE PRIVACY:|There is Secrecy.");
-}, 8800);
+  snck.q("THERE IS NO MORE PRIVACY:|There is ONLY Secrecy.");
+}, 8020);
+setTimeout(function(){
+  snck.q("Wake up. Don't be complicit in your own exploitation.");
+}, 12020);
