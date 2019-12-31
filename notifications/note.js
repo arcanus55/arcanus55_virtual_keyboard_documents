231518aa8e182a670987584443fac4b8c99a1882
Arcanus 55 Privacy Paranoid Vault <41128567+arcanus55@users.noreply.github.com>
2019-12-31 16:37:03 -0600

    Update note.js
    Listen, times are a-changin. Your entire existence is now digital. Take 55 seconds to think about that. Trust me, identity theft is a very dark rabbit hole. Realistically, could you survive months off-line? At what cost?
    If things go sideways you'll be needing a plan B. Inaction makes you a complicity volunteer, not a victim. You've been warned.
     
    A55 Privacy Paranoid Vault with Dead Mans Switch (Coming 2020)

--- notifications/note.js
+++ notifications/note.js
@@ -2,11 +2,12 @@
 var nNextSnck = 0;
 
 function nextSnck(){
-  var aMsg = ["Secret Now. Forever Secret","Can you keep a Secret?","Secure and Endure","There is no Privacy|There is only Secrecy"];
+  var aMsg = ["Being Hacked is an Existential Crisis", "Secret Now. Forever Secret","Can you keep a Secret?","Secure and Endure","Listen, times are a-changin|Your entire existence is now digital."];
   snck.q( aMsg[ nNextSnck++ ]);
 }
          
 setTimeout(nextSnck, 17600);
-setTimeout(nextSnck, 680000);
+setTimeout(nextSnck, 380000);
 setTimeout(nextSnck, 148000);
 setTimeout(nextSnck, 84000);
+setTimeout(nextSnck, 480000);
