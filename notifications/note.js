fd592219ab44907d0138de8145a36cd2eca9e097
Arcanus 55 Privacy Paranoid Vault <41128567+arcanus55@users.noreply.github.com>
2020-01-15 21:47:07 -0600

    Update note.js

--- notifications/note.js
+++ notifications/note.js
@@ -2,12 +2,11 @@
 var nNextSnck = 0;
 
 function nextSnck(){
-  var aMsg = ["Being Hacked is an Existential Crisis", "Secret Now. Forever Secret","Can you keep a Secret?","Secure and Endure","Listen, times are a-changin|Your entire existence is now digital","Take 55 seconds to think|about your privacy","Trust me, identity theft|is a very dark rabbit hole"];
+  var aMsg = ["Identity Theft is an Existential Crisis|Secure and Endure", "Secret Now. Forever Secret","Can you keep a Secret?","Listen, times are a-changin|Your entire existence is now digital","Take 55 seconds to think|about your privacy","Trust me, identity theft|is a very dark rabbit hole"];
   snck.q( aMsg[ nNextSnck++ ]);
 }
          
 setTimeout(nextSnck, 17600);
-setTimeout(nextSnck, 380000);
 setTimeout(nextSnck, 148000);
 setTimeout(nextSnck, 84000);
 setTimeout(nextSnck, 480000);
