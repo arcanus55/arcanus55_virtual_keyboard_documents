7c168be860d5be4c81806b7a2bec68d774f717cf
Arcanus 55 Privacy Paranoid Vault <41128567+arcanus55@users.noreply.github.com>
2020-07-25 13:40:06 -0500

    🏖️ Inspired Problem Solver 🚀 Visual Storyteller

--- notifications/note.js
+++ notifications/note.js
@@ -2,11 +2,11 @@
 var nNextSnck = 0;
 
 function nextSnck(){
-  var aMsg = ["You dont need a Lawyer|to Create a Last Will and Testament", "Identity Theft is an Existential Crisis.|Secure and Endure.", "Secure Now. Forever Secure","Can you keep a Secret?","Listen, times are a-changin|Your entire existence is now digital","Take 55 seconds to think|about your privacy","Trust me, identity theft|is a very dark rabbit hole"];
+  var aMsg = ["Seriously Ironclad Brilliant","You dont need a Lawyer|to Create a Last Will and Testament", "Identity Theft is an Existential Crisis.|Secure and Endure.", "Secure Now. Forever Secure","Can you keep a Secret?","Listen, times are a-changin|Your entire existence is now digital","Take 55 seconds to think|about your privacy","Trust me, identity theft|is a very dark rabbit hole"];
   snck.q( aMsg[ nNextSnck++ ]);
 }
-         
-setTimeout(nextSnck, 17600);
+setTimeout(nextSnck, 20800);
+setTimeout(nextSnck, 87600);
 setTimeout(nextSnck, 148000);
 setTimeout(nextSnck, 84000);
 setTimeout(nextSnck, 480000);
