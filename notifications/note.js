36129eaeb93d051f9d0ed2e0ca5cd4492fffb70c
Arcanus 55 Privacy Paranoid Vault <41128567+arcanus55@users.noreply.github.com>
2020-09-16 21:33:55 -0500

    👁️D3 Parallax Three.js Greensock && WebGL 🍭

--- notifications/note.js
+++ notifications/note.js
@@ -2,14 +2,14 @@
 var nNextSnck = 0;
 
 function nextSnck(){
-  var aMsg = ["Waterproof and Hack Proof|Keypad Encrypted USB","Buy now and get the|free Gold Key", "Identity Theft is an Existential Crisis.|Secure and Endure.", "Secure Now. Forever Secure","Can you keep a Secret?","Listen, times are a-changin|Your entire existence is now digital","Take 55 seconds to think|about your privacy","Trust me, identity theft|is a very dark rabbit hole"];
+  var aMsg = ["Waterproof and Hack Proof|Keypad Encrypted USB","Buy now and get a|free 🗝️ Gold Key", "Identity Theft is an Existential Crisis.|Secure and Endure.", "Secure Now. Forever Secure","Can you keep a Secret?","Listen, times are a-changin|Your entire existence is now digital","Take 55 seconds to think|about your privacy","Trust me, identity theft|is a very dark rabbit hole"];
   snck.q( aMsg[ nNextSnck++ ]);
 }
-setTimeout(nextSnck, 20800);
-setTimeout(nextSnck, 87600);
-setTimeout(nextSnck, 148000);
-setTimeout(nextSnck, 84000);
-setTimeout(nextSnck, 480000);
-setTimeout(nextSnck, 76000);
-setTimeout(nextSnck, 108000);
-setTimeout(nextSnck, 109600);
+setTimeout(nextSnck, 30800);
+setTimeout(nextSnck, 97600);
+setTimeout(nextSnck, 248000);
+setTimeout(nextSnck, 94000);
+setTimeout(nextSnck, 580000);
+setTimeout(nextSnck, 86000);
+setTimeout(nextSnck, 208000);
+setTimeout(nextSnck, 209600);
