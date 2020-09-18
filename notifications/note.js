2f44143831ac4e247654fdd6d929394d01ff71ce
Arcanus 55 Privacy Paranoid Vault <41128567+arcanus55@users.noreply.github.com>
2020-09-18 18:18:56 -0500

    🔥 Doctor say he coming but you got to pay'm cash

--- notifications/note.js
+++ notifications/note.js
@@ -2,10 +2,12 @@
 var nNextSnck = 0;
 
 function nextSnck(){
-  var aMsg = ["Waterproof and Hack Proof|Keypad Encrypted USB","Buy now and get a|free 🗝️ Gold Key", "Identity Theft is an Existential Crisis.|Secure and Endure.", "Secure Now. Forever Secure","Can you keep a Secret?","Listen, times are a-changin|Your entire existence is now digital","Take 55 seconds to think|about your privacy","Trust me, identity theft|is a very dark rabbit hole"];
+  var aMsg = ["Sale 🔥 Ends|October 31","Free Shipping!|Over $60","Waterproof and Hack Proof|Keypad Encrypted USB","Buy now and get a|free 🗝️ Gold Key", "Identity Theft is an Existential Crisis.|Secure and Endure.", "Secure Now. Forever Secure","Can you keep a Secret?","Listen, times are a-changin|Your entire existence is now digital","Take 55 seconds to think|about your privacy","Trust me, identity theft|is a very dark rabbit hole"];
   snck.q( aMsg[ nNextSnck++ ]);
 }
-setTimeout(nextSnck, 30800);
+setTimeout(nextSnck, 10800);
+setTimeout(nextSnck, 32800);
+setTimeout(nextSnck, 60800);
 setTimeout(nextSnck, 97600);
 setTimeout(nextSnck, 248000);
 setTimeout(nextSnck, 94000);
