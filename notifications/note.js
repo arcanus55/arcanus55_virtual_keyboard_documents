09846cb7af1ab17c7df1376294223416133e8e4b
Arcanus 55 Privacy Paranoid Vault <41128567+arcanus55@users.noreply.github.com>
2020-09-18 23:20:33 -0500

    🌴 Can you keep a secret? 🎲 Arcanus 55

--- notifications/note.js
+++ notifications/note.js
@@ -2,11 +2,11 @@
 var nNextSnck = 0;
 
 function nextSnck(){
-  var aMsg = ["Sale 🔥 Ends|October 31","Free 🚀 Shipping!|Over $60","Waterproof 🔑 Hack Proof|Keypad Encrypted USB","Buy now and get a|free 🗝️ Gold Key", "Identity Theft is an Existential Crisis.|Secure and Endure.", "Secure Now. Forever Secure","Can you keep a Secret?","Listen, times are a-changin|Your entire existence is now digital","Take 55 seconds to think|about your privacy","Trust me, identity theft|is a very dark rabbit hole"];
+  var aMsg = ["Sale 🔥 Ends|October 31","Free 🚀 Shipping!|Over $60","Waterproof 🔑 Hack Proof|Keypad Encrypted USB","Buy now and get a|free 🔑 Gold Key", "Identity Theft is an Existential Crisis.|Secure and Endure.", "Secure Now. Forever Secure","Can you keep a Secret?","Listen, times are a-changin|Your entire existence is now digital","Take 55 seconds to think|about your privacy","Trust me, identity theft|is a very dark rabbit hole"];
   snck.q( aMsg[ nNextSnck++ ]);
 }
-setTimeout(nextSnck, 10800);
-setTimeout(nextSnck, 32800);
+setTimeout(nextSnck, 11800);
+setTimeout(nextSnck, 34800);
 setTimeout(nextSnck, 60800);
 setTimeout(nextSnck, 97600);
 setTimeout(nextSnck, 248000);
