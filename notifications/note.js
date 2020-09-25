0ac1e104c60a4864ea0c3073bfb0c4a0c886d0da
Arcanus 55 Privacy Paranoid Vault <41128567+arcanus55@users.noreply.github.com>
2020-09-25 17:55:22 -0500

    Waterproof 🔑 Hack Proof

--- notifications/note.js
+++ notifications/note.js
@@ -2,16 +2,17 @@
 var nNextSnck = 0;
 
 function nextSnck(){
-  var aMsg = ["Sale 🔥 Ends|October 31","Free 🚀 Shipping!|Over $60","Waterproof 🔑 Hack Proof|Keypad Encrypted USB","Buy now and get a|free 🔑 Antique Key", "Identity Theft is an Existential Crisis.|Secure and Endure.", "Secure Now. Forever Secure","Can you keep a Secret?","Listen, times are a-changin|Your entire existence is now digital","Take 55 seconds to think|about your privacy","Trust me, identity theft|is a very dark rabbit hole"];
+  var aMsg = ["Sale 🔥 Ends|October 31","Free 🚀 Shipping!|Over $60","Waterproof 🔑 Hack Proof|Keypad Encrypted USB","Buy now and get a|free 🔑 Antique Key", "Identity Theft is an Existential Crisis.|Secure and Endure.", "Secure Now. Forever Secure","Can you keep a Secret?","Listen, times are a-changin|Your entire existence is now digital","Take 55 seconds to think 🎲|about your privacy","Trust me, identity theft|is a very dark rabbit hole","Need help? 🎯|Click Chat"];
   snck.q( aMsg[ nNextSnck++ ]);
 }
-setTimeout(nextSnck, 11800);
-setTimeout(nextSnck, 34800);
-setTimeout(nextSnck, 60800);
-setTimeout(nextSnck, 97600);
+setTimeout(nextSnck,  11800);
+setTimeout(nextSnck,  34800);
+setTimeout(nextSnck,  60800);
+setTimeout(nextSnck, 167600);
 setTimeout(nextSnck, 248000);
-setTimeout(nextSnck, 101000);
-setTimeout(nextSnck, 580000);
-setTimeout(nextSnck, 120000);
+setTimeout(nextSnck, 188000);
+setTimeout(nextSnck, 480000);
+setTimeout(nextSnck, 320000);
 setTimeout(nextSnck, 208000);
 setTimeout(nextSnck, 210600);
+setTimeout(nextSnck, 280600);
