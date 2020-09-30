1175ac09935b3742efd919f2d0af99cb30fe8115
Arcanus 55 Privacy Paranoid Vault <41128567+arcanus55@users.noreply.github.com>
2020-09-30 17:00:15 -0500

    😎 Oscillation Overthruster 🪐

--- notifications/note.js
+++ notifications/note.js
@@ -2,7 +2,7 @@
 var nNextSnck = 0;
 
 function nextSnck(){
-  var aMsg = ["Sale 🔥 Ends|October 31","Free 🚀 Shipping!|Over $60","Waterproof 🔑 Hack Proof|Keypad Encrypted USB","Buy now and get a|free 🔑 Antique Key", "Identity Theft is an Existential Crisis.|Secure and Endure.", "Secure Now. Forever Secure","Can you keep a Secret?","Listen, times are a-changin|Your entire existence is now digital","Take 55 seconds to think 🎲|about your privacy","Trust me, identity theft|is a very dark rabbit hole","Need help? 🎯|Click Chat"];
+  var aMsg = ["Sale 🔥 Ends|Soon","Free 🚀 Shipping!|Over $60","Waterproof 🔑 Hack Proof|Keypad Encrypted USB","Buy now and get a|free 🔑 Antique Key", "Identity Theft is an Existential Crisis.|Secure and Endure.", "Secure Now. Forever Secure","Can you keep a Secret?","Listen, times are a-changin|Your entire existence is now digital","Take 55 seconds to think 🎲|about your privacy","Trust me, identity theft|is a very dark rabbit hole","Need help? 🎯|Click Chat"];
   snck.q( aMsg[ nNextSnck++ ]);
 }
 setTimeout(nextSnck,  11800);
